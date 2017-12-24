f065677d3ec0c3e69af0842a9b36f45a704bf1d8
tz1006 <a@omg.tf>
2017-12-24 17:19:43 +0800

    Update trade.py

--- trade.py
+++ trade.py
@@ -2,6 +2,7 @@
 
 from ghost import Ghost, Session
 from bs4 import BeautifulSoup
+from PIL import Image
 import pytesseract
 
 ua = 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_1) AppleWebKit/602.2.14 (KHTML, like Gecko) Version/10.0.1 Safari/602.2.14'
@@ -13,25 +14,31 @@ se = Session(gh, user_agent=ua, wait_timeout=20, wait_callback=None, display=Tru
 
 def login(username, password):
     login = 'https://trade.cgws.com/cgi-bin/user/Login'
-    se.open(login)
-    se.evaluate("document.getElementById('fundAccount').value='%s'" % username)
-    se.evaluate("changeControls('normal');")
-    html = se.content
-    soup = BeautifulSoup(html, "html.parser")
-    keyboard = soup.select('tbody')[0]
-    keys = keyboard.select('td')
-    for i in range(len(keys)):
-        j = (i // 4) + 1
-        k = (i % 4) + 1
-        globals()['key'+keys[i].text] = "tbody > tr:nth-child(%s) > td:nth-child(%s)" % (j, k)
-    for i in password:
-        se.click(globals()['key'+i], expect_loading=False)
-    se.capture_to('s/a.png', selector='#ticketImg')
-    vimage = Image.open('s/a.png')
-    vcode = pytesseract.image_to_string(vimage)
-    print(vcode)
-    se.evaluate("document.getElementById('ticket').value='%s'" % vcode)
-    se.click('#submit', expect_loading=True)
+    c = 1
+    while c != 4:
+        se.open(login)
+        se.evaluate("document.getElementById('fundAccount').value='%s'" % username)
+        se.evaluate("changeControls('normal');")
+        login_html = se.content
+        login_soup = BeautifulSoup(login_html, "html.parser")
+        keyboard = login_soup.select('tbody')[0]
+        keys = keyboard.select('td')
+        for i in range(len(keys)):
+            j = (i // 4) + 1
+            k = (i % 4) + 1
+            globals()['key'+keys[i].text] = "tbody > tr:nth-child(%s) > td:nth-child(%s)" % (j, k)
+        for i in password:
+            se.click(globals()['key'+i], expect_loading=False)
+        se.capture_to('s/a.png', selector='#ticketImg')
+        vimage = Image.open('s/a.png')
+        vcode = pytesseract.image_to_string(vimage)
+        print(vcode)
+        se.evaluate("document.getElementById('ticket').value='%s'" % vcode)
+        se.click('#submit', expect_loading=True)
+        html = se.content
+        soup = BeautifulSoup(html, "html.parser")
+        c = len(soup.select('frame'))
+     
 
 def mystock():
     # Mystock
@@ -50,18 +57,66 @@ def myaccount():
     myaccount = 'https://trade.cgws.com/cgi-bin/stock/EntrustQuery?function=MyAccount'
     se.open(myaccount)
 
+def che():
+    che = 'https://trade.cgws.com/cgi-bin/stock/StockEntrust?function=StockCancel'
+    se.open(che)
+
 def sell(code, price, amount):
     sell = 'https://trade.cgws.com/cgi-bin/stock/StockEntrust?function=StockBusiness&type=S'
     se.open(sell)
-    if code[0]+code[1] =='60':
+    if str(code)[0]+str(code)[1] =='60':
         se.evaluate("document.getElementById('secuid').selectedIndex='1'")
     se.evaluate("document.getElementById('stkcode').value='%s'" % code)
     se.evaluate("document.getElementById('price').value='%s'" % price)
     se.evaluate("document.getElementById('amount').value='%s'" % amount)
     se.click('#submit')
+    se.click('#boxyEnterBtn')
+    se.show()
+    se.show()
+
+def buy(code, price, amount):
+    sell = 'https://trade.cgws.com/cgi-bin/stock/StockEntrust?function=StockBusiness&type=B'
+    se.open(sell)
+    if str(code)[0]+str(code)[1] =='60':
+        se.evaluate("document.getElementById('secuid').selectedIndex='1'")
+    se.evaluate("document.getElementById('stkcode').value='%s'" % code)
+    se.evaluate("document.getElementById('price').value='%s'" % price)
+    se.evaluate("document.getElementById('amount').value='%s'" % amount)
+    se.click('#submit')
+    se.click('#boxyEnterBtn')
+    se.show()
+    se.show()
+
+def hold(username, password):
+    hold = 'https://www.cgws.com/ccweb/osoa/views/personalcenter/assetview/centralizedtrading.html'
+    vcode = 'a'
+    while len(vcode) != 4:
+        se.open(hold)
+        se.sleep(1)
+        se.wait_for_selector('#password')
+        se.wait_for_selector('#fund_account')
+        #se.wait_for_selector('a.jjr_la')
+        se.wait_for_selector('.jjr_yzm > img:nth-child(2)')
+        se.show()
+        # Vcode
+        se.capture_to('s/v.png', selector='.jjr_yzm > img:nth-child(2)')
+        vimage = Image.open('s/v.png')
+        vcode = pytesseract.image_to_string(vimage)
+        print(vcode)
+    se.set_field_value('#ticket', vcode)
+    # Username
+    se.set_field_value('#fund_account', username)
+    # Password
+    se.set_field_value('#password', password)
     se.show()
 
 
+ 
+
+    
 user = ''
 pw = '102030'
+hold(user, pw)
 login(user, pw)
+
+buy(600123, 1.00, 100)
